8282a936f58cb40d1e5e8695bc2ba7233480544b
swt <swtywmx@via.cool>
2023-10-31 22:14:27 +0800

    数据库模块-修改配置信息

--- su-database/src/main/java/com/su/config/MybatisPlusLoadConfig.java
+++ su-database/src/main/java/com/su/config/MybatisPlusLoadConfig.java
@@ -8,5 +8,5 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @LoadFile(filePath = "classpath:/su-db.yml")
-public class LoadConfig {
+public class MybatisPlusLoadConfig {
 }
