1f1897bf93585448d152c899a68ad55a70505a36
swt <swtywmx@via.cool>
2023-10-31 22:39:37 +0800

    es模块-初始化

--- su-elasticsearch/src/main/java/com/su/config/EsLoadConfig.java
+++ su-elasticsearch/src/main/java/com/su/config/EsLoadConfig.java
@@ -1,10 +1,12 @@
 package com.su.config;
 
 import com.su.annotation.LoadFile;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author suweitao
  */
+@Configuration
 @LoadFile(filePath = "classpath:/su-es.yml")
 public class EsLoadConfig {
 }
