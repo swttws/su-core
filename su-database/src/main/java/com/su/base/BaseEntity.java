72c946a8ee8279cd5dddaadc39028385495b5bf1
swt <swtywmx@via.cool>
2023-10-31 22:25:48 +0800

    数据库模块-添加公共实体类和自动填充

---
+++ su-database/src/main/java/com/su/base/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.su.base;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author suweitao
+ * 公共字段
+ */
+@Data
+public class BaseEntity implements Serializable {
+
+    /**
+     * 逻辑删除字段，0未删除，1已删除
+     */
+    private Boolean isDelete;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    private LocalDateTime updateTime;
+
+    /**
+     * 版本号
+     */
+    private String version;
+
+}
---
+++ su-database/src/main/java/com/su/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.su.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * author suweitao
+ * @author 21781
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    }
+}
