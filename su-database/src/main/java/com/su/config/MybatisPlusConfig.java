1d78069a0c86615a5560fe780dd7e65e442b7d0a
swt <swtywmx@via.cool>
2023-10-31 21:47:08 +0800

    数据库模块配置设置

---
+++ su-database/src/main/java/com/su/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.su.config;
+
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author suweitao
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInnerInterceptor paginationInnerInterceptor(){
+        return new PaginationInnerInterceptor();
+    }
+
+}
